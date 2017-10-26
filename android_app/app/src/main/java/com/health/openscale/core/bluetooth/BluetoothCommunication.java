b2ec8599b7b17ee6c39dc0550ec0be1e42688fc7
Andreas Shimokawa <shimokawa@fsfe.org>
2017-10-26 23:07:57 +0200

    Initial working support for Mi Scale v2
    Notes:
    - This is basically a copy of BluetoothMiScale.java with some data offset adjustments
    - The absent realtime measurement characteristic disabled
    - No extra body fat etc infos are parsed, just the same data as on the v1 scale are processed
    - The scale is not yet switched to kg, it is still operated in catty and there is no HW switch!

--- android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothCommunication.java
+++ android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothCommunication.java
@@ -38,7 +38,7 @@ public abstract class BluetoothCommunication {
         BT_CONNECTION_LOST, BT_NO_DEVICE_FOUND, BT_UNEXPECTED_ERROR, BT_SCALE_MESSAGE
     };
     public enum BT_MACHINE_STATE {BT_INIT_STATE, BT_CMD_STATE, BT_CLEANUP_STATE}
-    public enum BT_DEVICE_ID {CUSTOM_OPENSCALE, MI_SCALE_V1, SANITAS_SBF70, MEDISANA_BS444, DIGOO_DGS038H, EXCELVANT_CF369BLE, YUNMAI_MINI}
+    public enum BT_DEVICE_ID {CUSTOM_OPENSCALE, MI_SCALE_V1, SANITAS_SBF70, MEDISANA_BS444, DIGOO_DGS038H, EXCELVANT_CF369BLE, YUNMAI_MINI, MI_SCALE_V2}
 
     protected Context context;
 
@@ -81,6 +81,8 @@ public abstract class BluetoothCommunication {
                 return new BluetoothCustomOpenScale(context);
             case MI_SCALE_V1:
                 return new BluetoothMiScale(context);
+            case MI_SCALE_V2:
+                return new BluetoothMiScale2(context);
             case SANITAS_SBF70:
                 return new BluetoothSanitasSbf70(context);
             case MEDISANA_BS444:
