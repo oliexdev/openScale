95b4686bb1bcb0db77ebb85769ca3beab5fdf616
OliE <olie.xdev@googlemail.com>
2017-11-11 14:45:43 +0100

    added about page

---
+++ android_app/app/src/main/java/com/health/openscale/gui/preferences/AboutPreferences.java
@@ -0,0 +1,35 @@
+/* Copyright (C) 2014  olie.xdev <olie.xdev@googlemail.com>
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+package com.health.openscale.gui.preferences;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.health.openscale.BuildConfig;
+import com.health.openscale.R;
+
+public class AboutPreferences extends PreferenceFragment {
+    private static final String KEY_APP_VERSION = "pref_app_version";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.about_preferences);
+
+        findPreference(KEY_APP_VERSION).setSummary("v" + BuildConfig.VERSION_NAME);
+    }
+}
