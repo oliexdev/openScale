c49cdeb12ae2efb49c870f23b9792807f837803f
Erik Johansson <erik@ejohansson.se>
2017-11-21 18:22:26 +0100

    Fix path to built apk

--- android_app/upload_apk.sh
+++ android_app/upload_apk.sh
@@ -1,2 +1,4 @@
-  # copy apk file to home
-  cp app/build/outputs/apk/app-debug.apk $HOME/openScale-dev-build.apk
+#!/bin/sh
+
+# copy apk file to home
+cp app/build/outputs/apk/debug/app-debug.apk $HOME/openScale-dev-build.apk
