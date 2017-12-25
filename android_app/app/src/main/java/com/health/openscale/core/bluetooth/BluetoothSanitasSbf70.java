48cd15a4049406adb89f459a28d1e3a0fce74c14
Matus Horvath <matus.horvath@gmail.com>
2017-12-25 03:09:07 +0100

    Add support for Silvercrest SBF75 scale (which seems to be a rebranded Sanitas SBF70).

--- android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothSanitasSbf70.java
+++ android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothSanitasSbf70.java
@@ -111,12 +111,26 @@ public class BluetoothSanitasSbf70 extends BluetoothCommunication {
 
     @Override
     public String deviceName() {
-        return "Sanitas SBF70";
+        return "Sanitas SBF70/SilverCrest SBF75";
     }
 
     @Override
     public String defaultDeviceName() {
         return "SANITAS SBF70";
+    }
+
+    @Override
+    public boolean checkDeviceName(String btDeviceName) {
+        if (super.checkDeviceName(btDeviceName)) {
+            return true;
+        }
+
+        // SilverCrest SBF75 (also known as HealthForYou by SilverCrest)
+        if (btDeviceName.toLowerCase().startsWith("sbf75")) {
+            return true;
+        }
+
+        return false;
     }
 
     @Override
