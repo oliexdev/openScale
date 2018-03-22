13d8850802c3e474c817265a8a4a200ec818e51f
Erik Johansson <erik@ejohansson.se>
2018-03-23 00:22:03 +0100

    Remove unused imports

--- android_app/app/src/main/java/com/health/openscale/gui/views/MeasurementView.java
+++ android_app/app/src/main/java/com/health/openscale/gui/views/MeasurementView.java
@@ -31,7 +31,6 @@ import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
