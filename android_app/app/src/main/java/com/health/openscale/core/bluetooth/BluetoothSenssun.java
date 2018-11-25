00362865a30f90e7a283938265579aa907d29924
Erik Johansson <erik@ejohansson.se>
2018-11-25 19:07:36 +0100

    Whitespace fixes

--- android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothSenssun.java
+++ android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothSenssun.java
@@ -36,7 +36,7 @@ public class BluetoothSenssun extends BluetoothCommunication {
     private final UUID CMD_MEASUREMENT_CHARACTERISTIC = BluetoothGattUuid.fromShortCode(0xfff2); // write only
 
     private int gotData;
-    private int FatMus=0;
+    private int FatMus = 0;
     private ScaleMeasurement measurement;
 
     public BluetoothSenssun(Context context) {
@@ -49,23 +49,23 @@ public class BluetoothSenssun extends BluetoothCommunication {
     }
 
     private void sendUserData(){
-      final ScaleUser selectedUser = OpenScale.getInstance().getSelectedScaleUser();
+        final ScaleUser selectedUser = OpenScale.getInstance().getSelectedScaleUser();
 
-      byte gender = selectedUser.getGender().isMale() ? (byte)0x01 : (byte)0xf1; // 00 - male; 01 - female
-      byte height = (byte)(((int)selectedUser.getBodyHeight()) & 0xff); // cm
-      byte age = (byte)(selectedUser.getAge() & 0xff);
+        byte gender = selectedUser.getGender().isMale() ? (byte)0x01 : (byte)0xf1; // 00 - male; 01 - female
+        byte height = (byte)(((int)selectedUser.getBodyHeight()) & 0xff); // cm
+        byte age = (byte)(selectedUser.getAge() & 0xff);
 
-      int userId = selectedUser.getId();
+        int userId = selectedUser.getId();
 
-      Timber.d("Request Saved User Measurements ");
-      byte cmdByte[] = {(byte)0xa5,(byte)0x10,gender,age,height,(byte)0,(byte)0x0,(byte)0x0d2,(byte)0x00};
+        Timber.d("Request Saved User Measurements ");
+        byte cmdByte[] = {(byte)0xa5, (byte)0x10, gender, age, height, (byte)0, (byte)0x0, (byte)0x0d2, (byte)0x00};
 
-      byte verify = 0;
-        for(int i=1;i<cmdByte.length-2;i++){
-          verify=(byte) (verify+cmdByte[i]);
+        byte verify = 0;
+        for(int i = 1; i < cmdByte.length - 2; i++) {
+            verify = (byte) (verify + cmdByte[i]);
         }
-      cmdByte[cmdByte.length-2]=verify;
-      writeBytes(WEIGHT_MEASUREMENT_SERVICE, CMD_MEASUREMENT_CHARACTERISTIC, cmdByte);
+        cmdByte[cmdByte.length - 2] = verify;
+        writeBytes(WEIGHT_MEASUREMENT_SERVICE, CMD_MEASUREMENT_CHARACTERISTIC, cmdByte);
     }
 
     @Override
@@ -90,21 +90,19 @@ public class BluetoothSenssun extends BluetoothCommunication {
 
     @Override
     protected boolean nextBluetoothCmd(int stateNr) {
-      switch (stateNr) {
-          case 0:
-          case 1:
-          case 2:
-          case 3:
-          case 4:
-          case 5:
-              sendUserData();
-
-              break;
-
-          default:
-              return false;
-      }
-      return true;
+        switch (stateNr) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                sendUserData();
+                break;
+            default:
+                return false;
+        }
+        return true;
     }
 
     @Override
@@ -119,65 +117,61 @@ public class BluetoothSenssun extends BluetoothCommunication {
         // The first notification only includes weight and all other fields are
         // either 0x00 (user info) or 0xff (fat, water, etc.)
 
-        if (data != null  ) {
+        if (data != null) {
             parseBytes(data);
-            if (measurement!=null && measurement.getWeight()!=0.0 && gotData==0){
-                    Timber.d("meas: %s",measurement.toString());
-              addScaleData(measurement);
-              gotData=1;
-              nextMachineStateStep();
+            if (measurement != null && measurement.getWeight() != 0.0 && gotData == 0) {
+                Timber.d("meas: %s",measurement.toString());
+                addScaleData(measurement);
+                gotData = 1;
+                nextMachineStateStep();
             }
-            if (measurement!=null && measurement.getWeight()!=0.0 && FatMus==0x03 &&  gotData!=2){
-                    Timber.d("meas: %s",measurement.toString());
-              addScaleData(measurement);
-              gotData=2;
-
+            if (measurement != null && measurement.getWeight() != 0.0 && FatMus == 0x03 && gotData != 2) {
+                Timber.d("meas: %s",measurement.toString());
+                addScaleData(measurement);
+                gotData = 2;
             }
         }
     }
 
     private void parseBytes(byte[] weightBytes) {
-      if (measurement==null){
-        measurement= new ScaleMeasurement();
-      }
-      int type=(int)weightBytes[6]&0xff;
-      Timber.d("type %02X",type);
-      switch (type) {
-        case 0xaa:
-          float weight = Converters.fromUnsignedInt16Be(weightBytes, 2) / 10.0f; // kg
-          measurement.setWeight(weight);
-          break;
-        case 0xb0:
-          float fat = Converters.fromUnsignedInt16Be(weightBytes, 2) / 10.0f; // %
-          float water = Converters.fromUnsignedInt16Be(weightBytes, 4) / 10.0f; // %
-          measurement.setFat(fat);
-          measurement.setWater(water);
-          FatMus|=0x2;
-          break;
-        case 0xc0:
-          float bone = Converters.fromUnsignedInt16Le(weightBytes, 4) / 10.0f; // kg
-          float muscle = Converters.fromUnsignedInt16Be(weightBytes, 2) / 10.0f; // %
-          measurement.setMuscle(muscle);
-          measurement.setBone(bone);
-          FatMus|=0x1;
-          break;
-        case 0xd0:
-          float calorie = Converters.fromUnsignedInt16Be(weightBytes, 2);
-          break;
-        case 0xe0:
-          break;
-        case 0xe1:
-          break;
-        case 0xe2:
-          //date
-          break;
-        default:
-
-      }
+        if (measurement == null) {
+            measurement = new ScaleMeasurement();
+        }
+        int type = (int)weightBytes[6] & 0xff;
+        Timber.d("type %02X",type);
+        switch (type) {
+            case 0xaa:
+                float weight = Converters.fromUnsignedInt16Be(weightBytes, 2) / 10.0f; // kg
+                measurement.setWeight(weight);
+                break;
+            case 0xb0:
+                float fat = Converters.fromUnsignedInt16Be(weightBytes, 2) / 10.0f; // %
+                float water = Converters.fromUnsignedInt16Be(weightBytes, 4) / 10.0f; // %
+                measurement.setFat(fat);
+                measurement.setWater(water);
+                FatMus |= 0x2;
+                break;
+            case 0xc0:
+                float bone = Converters.fromUnsignedInt16Le(weightBytes, 4) / 10.0f; // kg
+                float muscle = Converters.fromUnsignedInt16Be(weightBytes, 2) / 10.0f; // %
+                measurement.setMuscle(muscle);
+                measurement.setBone(bone);
+                FatMus |= 0x1;
+                break;
+            case 0xd0:
+                float calorie = Converters.fromUnsignedInt16Be(weightBytes, 2);
+                break;
+            case 0xe0:
+                break;
+            case 0xe1:
+                break;
+            case 0xe2:
+                //date
+                break;
+            default:
+        }
 
         //measurement.setDateTime(lastWeighted);
         measurement.setDateTime(new Date());
-
-
     }
 }
