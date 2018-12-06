e7e4e5016edba13c376d8185d715ad1fa430741b
oliexdev <oliex.dev@googlemail.com>
2018-12-06 07:23:53 +0100

    revert test device name

--- android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothFactory.java
+++ android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothFactory.java
@@ -81,7 +81,7 @@ public class BluetoothFactory {
         if (name.startsWith("SANITAS SBF70".toLowerCase(Locale.US)) || name.startsWith("sbf75")) {
             return new BluetoothBeurerSanitas(context, BluetoothBeurerSanitas.DeviceType.SANITAS_SBF70_70);
         }
-        if (deviceName.startsWith("YUNMAI-SIGNAL?-") || deviceName.startsWith("YUNMAI-ISM2-")) {
+        if (deviceName.startsWith("YUNMAI-SIGNAL-") || deviceName.startsWith("YUNMAI-ISM2-")) {
             return new BluetoothYunmaiSE_Mini(context, true);
         }
         if (deviceName.startsWith("YUNMAI-ISSE")) {
