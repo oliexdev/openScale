1efbb05c4acc5d15f7e7db299d9a3c98ecabb702
Sunguk Lee <d3m3vilurr@gmail.com>
2019-12-03 01:23:29 +0900

    Support activity level in Yunmai scales and clean up YunmaiLib
    this patch needs to confirm from newer version users and fix the muscle value of
    normal body users
    
    a getMuscle function doesn't be related `sex`, only related `body_type`
    in the original app, if body_type is 1(fitness body type), it should multiply
    with 0.7.
    if not (normal body type), it should multiply with 0.67

--- android_app/app/src/main/java/com/health/openscale/core/bluetooth/lib/YunmaiLib.java
+++ android_app/app/src/main/java/com/health/openscale/core/bluetooth/lib/YunmaiLib.java
@@ -16,13 +16,27 @@
 
 package com.health.openscale.core.bluetooth.lib;
 
+import com.health.openscale.core.utils.Converters.ActivityLevel;
+
 public class YunmaiLib {
     private int sex; // male = 1; female = 0
     private float height;
+    private boolean fitnessBodyType;
 
-    public YunmaiLib(int sex, float height) {
+    static public int toYunmaiActivityLevel(ActivityLevel activityLevel) {
+        switch (activityLevel) {
+            case HEAVY:
+            case EXTREME:
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    public YunmaiLib(int sex, float height, ActivityLevel activityLevel) {
         this.sex = sex;
         this.height = height;
+        this.fitnessBodyType = YunmaiLib.toYunmaiActivityLevel(activityLevel) == 1;
     }
 
     public float getWater(float bodyFat) {
@@ -40,13 +54,16 @@ public class YunmaiLib {
             r = (float)Math.sqrt(r);
         }
 
+        fat = (weight * 1.5f / h / h) + (age * 0.08f);
         if (this.sex == 1) {
-            fat = (weight * 1.5f / h / h) + (age * 0.08f) - 10.8f;
-        } else {
-            fat = (weight * 1.5f / h / h) + (age * 0.08f);
+            fat -= 10.8f;
         }
 
         fat = (fat - 7.4f) + r;
+
+        if (fat < 5.0f || fat > 75.0f) {
+            fat = 0.0f;
+        }
 
         return fat;
     }
@@ -55,7 +72,7 @@ public class YunmaiLib {
         float muscle;
         muscle = (100.0f - bodyFat) * 0.67f;
 
-        if (sex == 1) {
+        if (this.fitnessBodyType) {
             muscle = (100.0f - bodyFat) * 0.7f;
         }
 
@@ -63,6 +80,20 @@ public class YunmaiLib {
 
         return muscle;
     }
+
+    public float getSkeletalMuscle(float bodyFat) {
+        float muscle;
+
+        muscle = (100.0f - bodyFat) * 0.53f;
+        if (this.fitnessBodyType) {
+            muscle = (100.0f - bodyFat) * 0.6f;
+        }
+
+        muscle = (muscle * 100.0f) + 0.5f / 100.0f;
+
+        return muscle;
+    }
+
 
     public float getBoneMass(float muscle, float weight) {
         float boneMass;
@@ -81,46 +112,59 @@ public class YunmaiLib {
     }
 
     public float getLeanBodyMass(float weight, float bodyFat) {
-        if (bodyFat < 5.0f || bodyFat > 75.0f) {
-            return 0.0f;
-        }
         return weight * (100.0f - bodyFat) / 100.0f;
     }
 
     public float getVisceralFat(float bodyFat, int age) {
-        float f = (bodyFat < 5.0f || bodyFat > 75.0f) ? 0.0f : bodyFat;
+        float f = bodyFat;
         int a = (age < 18 || age > 120) ? 18 : age;
 
-        if (sex == 1) {
-            if (a < 40) {
-                f -= 21.0f;
-            } else if (a < 60) {
-                f -= 22.0f;
+        float vf;
+        if (!fitnessBodyType) {
+            if (sex == 1) {
+                if (a < 40) {
+                    f -= 21.0f;
+                } else if (a < 60) {
+                    f -= 22.0f;
+                } else {
+                    f -= 24.0f;
+                }
             } else {
-                f -= 24.0f;
+                if (a < 40) {
+                    f -= 34.0f;
+                } else if (a < 60) {
+                    f -= 35.0f;
+                } else {
+                    f -= 36.0f;
+                }
             }
+
+            float d = sex == 1 ? 1.4f : 1.8f;
+            if (f > 0.0f) {
+                d = 1.1f;
+            }
+
+            vf = (f / d) + 9.5f;
+            if (vf < 1.0f) {
+                return 1.0f;
+            }
+            if (vf > 30.0f) {
+                return 30.0f;
+            }
+            return vf;
         } else {
-            if (a < 40) {
-                f -= 34.0f;
-            } else if (a < 60) {
-                f -= 35.0f;
+            if (bodyFat > 15.0f) {
+                vf = (bodyFat - 15.0f) / 1.1f + 12.0f;
             } else {
-                f -= 36.0f;
+                vf = -1 * (15.0f - bodyFat) / 1.4f + 12.0f;
             }
+            if (vf < 1.0f) {
+                return 1.0f;
+            }
+            if (vf > 9.0f) {
+                return 9.0f;
+            }
+            return vf;
         }
-
-        float d = sex == 1 ? 1.4f : 1.8f;
-        if (f > 0.0f) {
-            d = 1.1f;
-        }
-
-        float vf = (f / d) + 9.5f;
-        if (vf < 1.0f) {
-            return 1.0f;
-        }
-        if (vf > 30.0f) {
-            return 30.0f;
-        }
-        return vf;
     }
 }
