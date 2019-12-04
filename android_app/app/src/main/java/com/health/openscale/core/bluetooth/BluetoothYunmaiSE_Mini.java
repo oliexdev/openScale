6d0e2d18556ef6a8b3cf37eefbbee0968d8ab410
Sunguk Lee <d3m3vilurr@gmail.com>
2019-12-04 23:35:49 +0900

    Add YUNMAI LBM & visceral fat value. (#522)
    * Add YUNMAI LBM value
    
    this scale machine just use `weight - fat` formula
    
    * Initial implement YunmaiLib.getVisceralFat
    
    a return value range is 1 to 30
    an original app use `floor` calculation to last value and return the integer value.
    it's a different part.

--- android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothYunmaiSE_Mini.java
+++ android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothYunmaiSE_Mini.java
@@ -145,6 +145,8 @@ public class BluetoothYunmaiSE_Mini extends BluetoothCommunication {
                 scaleBtData.setMuscle(yunmaiLib.getMuscle(bodyFat));
                 scaleBtData.setWater(yunmaiLib.getWater(bodyFat));
                 scaleBtData.setBone(yunmaiLib.getBoneMass(scaleBtData.getMuscle(), weight));
+                scaleBtData.setLbm(yunmaiLib.getLeanBodyMass(weight, bodyFat));
+                scaleBtData.setVisceralFat(yunmaiLib.getVisceralFat(bodyFat, scaleUser.getAge()));
             } else {
                 Timber.e("body fat is zero");
             }
