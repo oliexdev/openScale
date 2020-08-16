ddb47abde276f314dd7402c406963b5e68946aa4
oliexdev <olie.xdev@googlemail.com>
2020-08-16 20:09:34 +0200

    refresh graph on overview action bar clicks

--- android_app/app/src/main/java/com/health/openscale/gui/overview/OverviewFragment.java
+++ android_app/app/src/main/java/com/health/openscale/gui/overview/OverviewFragment.java
@@ -100,6 +100,7 @@ public class OverviewFragment extends Fragment {
         chartActionBarView.setOnActionClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                chartView.refreshMeasurementList();
                 updateChartView();
             }
         });
