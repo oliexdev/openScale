5e041b956b0c4e60e0076d512293fae482e6555d
hyx0329 <hyx0329@outlook.com>
2023-02-20 17:32:30 +0800

    Add support for Yoda1 (#927)

--- android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothFactory.java
+++ android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothFactory.java
@@ -136,6 +136,9 @@ public class BluetoothFactory {
         if (deviceName.equals("CH100")){
             return new BluetoothHuaweiAH100(context);
         }
+        if (deviceName.equals("Yoda1")){
+            return new BluetoothYoda1Scale(context);
+        }
         return null;
     }
 }
